6c3348348905ad38cd03d32579a909dc8ab1ee60
Malthorn1 <mikkelskott@gmail.com>
2020-06-16 04:40:46 +0200

    tests

---
+++ src/test/java/rest/CourseRessourceTest.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entities.Course;
+import entities.YogaClass;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import java.net.URI;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
+import utils.EMF_Creator.DbSelector;
+import utils.EMF_Creator.Strategy;
+
+/**
+ *
+ * @author mikke
+ */
+
+
+public class CourseRessourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+    private static Course c1, c2, c3;
+    private static YogaClass yc1;
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        //This method must be called before you request the EntityManagerFactory
+        EMF_Creator.startREST_TestWithDB();
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.CREATE);
+
+        httpServer = startServer();
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //System.in.read();
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+        httpServer.shutdownNow();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        c1 = new Course("Flexible Yoga", "Flexible");
+        c2 = new Course("Hot Yoga", "Hot");
+        c3 = new Course("Cold Yoga", "Cold");
+
+        yc1 = new YogaClass(15,"20/02-2020", 300);
+        try {
+            em.getTransaction().begin();
+          
+            em.createNamedQuery("Instructor.deleteAllRows").executeUpdate();
+            em.createNamedQuery("SignedUp.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Student.deleteAllRows").executeUpdate();
+            em.createNamedQuery("YogaClass.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Course.deleteAllRows").executeUpdate();
+            em.persist(c1);
+            em.persist(c2);
+            em.persist(c3);
+            em.persist(yc1);
+            yc1.setCourse(c3);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+    
+      @Test
+    public void testServerIsUp() {
+        System.out.println("Testing is server UP");
+        given().when().get("/course").then().statusCode(200);
+    }
+    
+       //GET
+
+
+
+
+}
