3e62bc750c8225717872e87fdf723f5cd9e1af9d
Malthorn1 <mikkelskott@gmail.com>
2020-06-16 02:57:01 +0200

    travis

--- src/main/java/rest/populateDBResource.java
+++ src/main/java/rest/populateDBResource.java
@@ -2,19 +2,13 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import dtos.CatFactDTO;
 import utils.EMF_Creator;
-import utils.SetupTestUsers; 
-import facades.FacadeExample;
 import facades.UserFacade;
-import java.io.IOException;
-import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import utils.HttpUtils;
 
 //Todo Remove or change relevant parts before ACTUAL use
 
